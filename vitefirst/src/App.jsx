7cefd677b7c484a31d1ea584f6d4e14554fa62d3
Tanmay-Giram <tanmaygiram123@gmail.com>
2024-08-09 20:24:27 +0530

    initial commit

---
+++ vitefirst/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+
+
+function App() {
+
+  let [counter,setCounter] = useState(10)
+
+  const addValue =() => {
+    setCounter(counter+1);
+  }
+
+  const subtractValue =() => {
+    setCounter(counter-1);
+  }
+
+  return (
+<div>
+  <h2>count is:{counter}</h2>
+  <button onClick={addValue}>adder</button>
+  <button onClick={subtractValue}>subtract</button>
+</div>
+  )
+}
+
+export default App
